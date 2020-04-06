4ab44b22fe2c9afa75bf20a1627c1e2a8a5dda78
Lucas Amaral <lopesdoamaral@gmail.com>
2020-04-06 16:19:25 -0300

    EOF

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,11 +1,9 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage {
     @FindBy(how = How.CLASS_NAME, using = "alert-success")
