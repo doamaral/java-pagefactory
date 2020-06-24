f9e373702f986e5193e35cb5223abdf034be4caf
unknown <lopesdoamaral@gmail.com>
2020-06-24 13:37:55 -0300

    Singleton Strategy to implement Driver Factory
    Using a Lazy Singleton no thread safe strategy to Driver Factory

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,8 @@
 package tests;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
 import driver.SimpleDriverFactory;
+import driver.SingletonDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -10,7 +12,12 @@ public class BaseTest {
 
     @BeforeTest
     public void setUp(){
-        nav = SimpleDriverFactory.getFirefoxDriver();
+        nav = SingletonDriverFactory.getFirefoxDriver();
+        System.out.println(SingletonDriverFactory.numberOfInstances);
+        WebDriver nav2 = SingletonDriverFactory.getFirefoxDriver();
+        System.out.println(SingletonDriverFactory.numberOfInstances);
+        WebDriver nav3 = SingletonDriverFactory.getFirefoxDriver();
+        System.out.println(SingletonDriverFactory.numberOfInstances);
         nav.get("http://seubarriga.wcaquino.me/login");
     }
 
